f654d7a080a984570d71db301d31c6a2e41ae04a
juvenal-yescas <juvenal@mail.com>
2022-12-06 09:18:30 -0600

    Update ignore file, save ip list to file

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/lixiangzhong/dnsutil"
@@ -10,7 +11,92 @@ import (
 )
 
 func main() {
-	fmt.Println(getAAAARecordsNTimes("youtube.com", 4))
+	AAAARecords, err := getAAAARecordsNTimes("youtube.com", 4)
+	if err != nil {
+		return
+	}
+	createFileIfNotExists("test.txt")
+	ipListFromFile, err := readLines("test.txt")
+	if err != nil {
+		return
+	}
+
+	// merge 2 slices
+	ipList := append(AAAARecords, ipListFromFile...)
+	// remove duplicates
+	ipList = unique(ipList)
+
+	// write to file
+	writeToFile("test.txt", ipList)
+}
+
+// write to file slice string line by line
+func writeToFile(filename string, data []string) error {
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	defer file.Close()
+
+	for _, d := range data {
+		if _, err := file.WriteString(d + "\n"); err != nil {
+			log.Error(err)
+			return err
+		}
+	}
+	return nil
+}
+
+// from array get onley unique values
+func unique(intSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
+// read all lines from file and return a slice of string
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// create file txt if not exists
+func createFileIfNotExists(filename string) error {
+	if !fileExists(filename) {
+		file, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+	}
+	return nil
+}
+
+// Check if file exists
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
 // get N times AAAA records for a domain
@@ -28,26 +114,15 @@ func getAAAARecordsNTimes(domain string, n int) ([]string, error) {
 		}
 	}
 
-	// remove duplicate ip
-	uniqueIp := make(map[string]bool)
-	var cleanListIp = []string{}
-
-	for _, val := range AAAARecord {
-		if _, ok := uniqueIp[val]; !ok {
-			uniqueIp[val] = true
-			cleanListIp = append(cleanListIp, val)
-		}
-	}
-
-	return cleanListIp, nil
+	return unique(AAAARecord), nil
 }
 
 // get AAAA records for a domain
 func AAAA(domain, dnsIp string) ([]string, error) {
 
 	var dig dnsutil.Dig
-	dig.SetDNS(dnsIp)                 //or ns.xxx.com
-	a, err := dig.AAAA("youtube.com") // dig google.com @8.8.8.8
+	dig.SetDNS(dnsIp)          //or ns.xxx.com
+	a, err := dig.AAAA(domain) // dig google.com @8.8.8.8
 	if err != nil {
 		log.Error(err)
 		return nil, err
