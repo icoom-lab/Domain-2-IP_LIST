2b73f496bfdbb3358892afae8a7686eefd57c665
juvenal-yescas <juvenal@mail.com>
2022-12-13 15:29:33 -0600

    Dns cloudflare

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func fileExists(filename string) bool {
 func GetAAAARecordsNTimes(domain string, n int) ([]string, error) {
 	AAAARecord := make([]string, 0)
 
-	listDns := []string{"8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844"}
+	listDns := []string{"8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844", "1.1.1.1", "2606:4700:4700::1111"}
 	for i := 0; i < n; i++ {
 
 		rand.Seed(time.Now().Unix())
